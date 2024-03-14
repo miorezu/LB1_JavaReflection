40254405dde72890b6ed7ac3530ebe71679f6de5
miorezu <m.miorezu@gmail.com>
2024-03-14 20:45:38 +0200

    Second task

--- src/ClassInfo.java
+++ src/ClassInfo.java
@@ -1,40 +1,65 @@
-/*ім'я пакета1, модифікатори та ім'я аналізованого класу1, його базовий клас1, список
-        реалізованих інтерфейсів1, список усіх полів1, конструкторів та методів1, та їх характеристики.*/
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Scanner;
+
 public abstract class ClassInfo {
-    public static void getInfo(Class<?> myClass){
+    public static void getInfo(Class<?> myClass) {
+
         StringBuilder infoClass = new StringBuilder();
         infoClass.append("Package: " + myClass.getPackage() + "\n");
-        infoClass.append("Name: " + myClass + "\n");
-        infoClass.append("Modifiers: " + myClass.getModifiers() + "\n");
+        infoClass.append("Name: " + myClass.getSimpleName() + "\n");
+        infoClass.append("Modifiers: " + Modifier.toString(myClass.getModifiers()) + "\n");
         infoClass.append("Base class: " + myClass.getSuperclass() + "\n");
         infoClass.append("Implemented interfaces: ");
         Class<?>[] interfaces = myClass.getInterfaces();
-        if (interfaces.length > 0)
-        {
+        if (interfaces.length > 0) {
             for (Class<?> listOfInterfaces : interfaces) {
                 infoClass.append(listOfInterfaces.getName()).append("\n\t\t\t\t\t\t");
             }
-        }else {
+        } else {
             infoClass.append("None");
         }
         infoClass.append("\nDeclared fields: " + Arrays.toString(myClass.getDeclaredFields()) + "\n");
         infoClass.append("\nDeclared methods: ");
-        Method[] declaredMethods =
-                myClass.getDeclaredMethods();
+        Method[] declaredMethods = myClass.getDeclaredMethods();
         for (Method method : declaredMethods) {
             infoClass.append(method + "\n");
         }
         infoClass.append("\nConstructors: " + Arrays.toString(myClass.getConstructors()) + "\n");
-
-
-
-
-
-
-
-
         System.out.println(infoClass);
+    }
+
+    public static void callInfo(University.Student newStudent) throws InvocationTargetException, IllegalAccessException {
+
+        StringBuilder callIn = new StringBuilder();
+        callIn.append("Name of class: " + newStudent.getClass().getSimpleName() + "\n");
+        callIn.append("\nDeclared fields: " + Arrays.toString(newStudent.getClass().getDeclaredFields()) + "\n");
+        String group = newStudent.getGroup();
+        String name = null;
+        String surname = null;
+        try {
+            Field field = newStudent.getClass().getDeclaredField("name");
+            field.setAccessible(true);
+            name = (String) field.get(newStudent);
+            field = newStudent.getClass().getDeclaredField("surname");
+            field.setAccessible(true);
+            surname = (String) field.get(newStudent);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        callIn.append(name + " " + surname + "\t" + group);
+        System.out.println(callIn);
+        System.out.println("Choose method to call:\n");
+        Method[] methods = newStudent.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method);
+        }
+        System.out.println("Print number of method: ");
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+        System.out.println(methods[option - 1].invoke(newStudent));
     }
 }
--- src/University/Student.java
+++ src/University/Student.java
@@ -2,17 +2,34 @@ package University;
 
 import javax.xml.crypto.Data;
 import java.io.Serializable;
+import java.util.Scanner;
 
 public class Student implements Serializable, Data {
     private String name;
+    private String surname;
     public String group;
+
     public Student() {
     }
-    public Student(String name, String group) {
+
+    public Student(String name, String surname, String group) {
         this.name = name;
+        this.surname = surname;
         this.group = group;
     }
+
     public String getGroup() {
         return group;
+    }
+
+    public String getSName() {
+        return name + " " + surname;
+    }
+
+    public void rate() {
+        System.out.println("What rating would you like to give?");
+        Scanner scanner = new Scanner(System.in);
+        int mark = scanner.nextInt();
+        System.out.println("You rated it - " + mark);
     }
 }
